4e9353a75a867cb68515fce135a4f2987f22ece6
Oliver Lau <oliver@ersatzworld.net>
2023-01-02 09:28:25 +0100

    Initial Commit

---
+++ wiggle/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  wiggle
+//
+//  Created by Oliver Lau on 02.01.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
